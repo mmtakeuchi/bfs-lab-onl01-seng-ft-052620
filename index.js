7f4a38b6baba3444b48d2aab777c2ea437f846ce
mmtakeuchi <mikemtaki@gmail.com>
2021-01-26 15:54:52 -0700

    commit tree b8f197359083f7935f20d7c4f17034e3bebb841f

--- index.js
+++ index.js
@@ -1,3 +1,44 @@
-function bfs(rootNode, vertices, edges){
+const bfs = (rootNode, vertices, edges) => {
+  rootNode.distance = 0;
+  let discovered = [rootNode];
+  let discoverOrder = [rootNode];
+  while (discovered.length != 0) {
+    let currentNode = discovered.shift();
+    let adjacentNodes = findAdjacent(currentNode.name, vertices, edges);
+    discoverOrder = discoverOrder.concat(adjacentNodes);
+    markDistanceAndPredecessor(currentNode, adjacentNodes);
+    discovered = discovered.concat(adjacentNodes);
+  }
+  return discoverOrder;
+};
 
-}
+const findAdjacent = (nodeName, vertices, edges) => {
+  return edges
+    .filter((edge) => {
+      return edge.includes(nodeName);
+    })
+    .map((edge) => {
+      return edge.filter((node) => {
+        return node != nodeName;
+      })[0];
+    })
+    .map((name) => {
+      return findNode(name, vertices);
+    })
+    .filter((node) => {
+      return node.distance == null;
+    });
+};
+
+const markDistanceAndPredecessor = (predecessor, adjacentNodes) => {
+  adjacentNodes.map((node) => {
+    node.distance = predecessor.distance + 1;
+    node.predecessor = predecessor;
+  });
+};
+
+const findNode = (nodeName, vertices) => {
+  return vertices.find((vertex) => {
+    return vertex.name == nodeName;
+  });
+};
